473527c9ea8cffbb995daa3c70c6ad0b7644610f
Yggralith0 <coolman_357@hotmail.com>
2018-02-05 15:42:26 +0800

    add webservice

---
+++ IS3102_WebService-Student/IS3102_WebService-Student/src/java/service/CountryentityFacadeREST.java
@@ -0,0 +1,120 @@
+package service;
+
+import Entity.Countryentity;
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+import javax.ejb.Stateless;
+import javax.persistence.*;
+import javax.ws.rs.*;
+import javax.ws.rs.core.GenericEntity;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+@Stateless
+@Path("entity.countryentity")
+public class CountryentityFacadeREST extends AbstractFacade<Countryentity> {
+
+    @PersistenceContext(unitName = "WebService")
+    private EntityManager em;
+
+    public CountryentityFacadeREST() {
+        super(Countryentity.class);
+    }
+
+    @POST
+    @Override
+    @Consumes({"application/xml", "application/json"})
+    public void create(Countryentity entity) {
+        super.create(entity);
+    }
+
+    @PUT
+    @Path("{id}")
+    @Consumes({"application/xml", "application/json"})
+    public void edit(@PathParam("id") Long id, Countryentity entity) {
+        super.edit(entity);
+    }
+
+    @DELETE
+    @Path("{id}")
+    public void remove(@PathParam("id") Long id) {
+        super.remove(super.find(id));
+    }
+
+    @GET
+    @Path("{id}")
+    @Produces({"application/xml", "application/json"})
+    public Countryentity find(@PathParam("id") Long id) {
+        return super.find(id);
+    }
+
+    @GET
+    @Override
+    @Produces({"application/xml", "application/json"})
+    public List<Countryentity> findAll() {
+        return super.findAll();
+    }
+
+    @GET
+    @Path("{from}/{to}")
+    @Produces({"application/xml", "application/json"})
+    public List<Countryentity> findRange(@PathParam("from") Integer from, @PathParam("to") Integer to) {
+        return super.findRange(new int[]{from, to});
+    }
+
+    @GET
+    @Path("count")
+    @Produces("text/plain")
+    public String countREST() {
+        return String.valueOf(super.count());
+    }
+
+    @GET
+    @Path("country")
+    @Produces({"application/json"})
+    public List<Countryentity> listAllCountries() {
+        Query q = em.createQuery("Select c from Countryentity c");
+        List<Countryentity> list = q.getResultList();
+        List<Countryentity> countryList = new ArrayList();
+        for (Countryentity country : list) {
+            em.detach(country);
+            country.setItemCountryentityList(null);
+            country.setMemberentityList(null);
+            country.setStoreentityList(null);
+            country.setWarehouseentityList(null);
+            countryList.add(country);
+        }
+        return countryList;
+    }
+    
+    @GET
+    @Path("getQuantity")
+    @Produces({"application/json"})
+    public Response getQuantity(@QueryParam("SKU") String SKU)
+    {
+        try
+        {
+            Connection conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/islandfurniture-it07?zeroDateTimeBehavior=convertToNull&user=root&password=12345");
+            String stmt = "Select li.QUANTITY as quantity from country_ecommerce c, warehouseentity w, storagebinentity sb, storagebinentity_lineitementity sbli, lineitementity li, itementity i where li.ITEM_ID=i.ID and sbli.lineItems_ID=li.ID and sb.ID=sbli.StorageBinEntity_ID and w.id=sb.WAREHOUSE_ID and c.warehouseentity_id=w.id and sb.type<>'Outbound' and i.SKU=?";
+            PreparedStatement ps = conn.prepareStatement(stmt);
+            ps.setString(1, SKU);
+            ResultSet rs = ps.executeQuery();
+            int qty = 0;
+            if (rs.next()) {
+                qty = rs.getInt("quantity");
+            }
+            return Response.ok(qty + "", MediaType.APPLICATION_JSON).build();
+            
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+            return Response.status(Response.Status.NOT_FOUND).build();
+        }
+    }
+
+    @Override
+    protected EntityManager getEntityManager() {
+        return em;
+    }
+
+}
---
+++ IS3102_WebService-Student/IS3102_WebService-Student/src/java/service/RetailproductentityFacadeREST.java
@@ -0,0 +1,146 @@
+package service;
+
+import Entity.RetailProduct;
+import Entity.Retailproductentity;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.List;
+import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
+import javax.ws.rs.core.GenericEntity;
+import javax.ws.rs.core.Response;
+
+@Stateless
+@Path("entity.retailproductentity")
+public class RetailproductentityFacadeREST extends AbstractFacade<Retailproductentity> {
+
+    @PersistenceContext(unitName = "WebService")
+    private EntityManager em;
+
+    public RetailproductentityFacadeREST() {
+        super(Retailproductentity.class);
+    }
+
+    @POST
+    @Override
+    @Consumes({"application/xml", "application/json"})
+    public void create(Retailproductentity entity) {
+        super.create(entity);
+    }
+
+    @GET
+    @Path("getRetailProductList")
+    @Produces("application/json")
+    public Response getRetailProductList(@QueryParam("countryID") Long countryID) {
+        System.out.println("RESTful: getRetailProductList() called with countryID " + countryID);
+        try {
+            List<RetailProduct> list = new ArrayList<>();
+            String stmt = "";
+            PreparedStatement ps;
+            Connection conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/islandfurniture-it07?zeroDateTimeBehavior=convertToNull&user=root&password=12345");
+
+            if (countryID == null) {
+                stmt = "SELECT i.ID as id, i.NAME as name, rp.IMAGEURL as imageURL, i.SKU as sku, i.DESCRIPTION as description, i.TYPE as type, i.CATEGORY as category FROM retailproductentity rp, itementity i where rp.ID=i.ID and i.ISDELETED=FALSE;";
+                ps = conn.prepareStatement(stmt);
+            } else {
+                stmt = "SELECT i.ID as id, i.NAME as name, rp.IMAGEURL as imageURL, i.SKU as sku, i.DESCRIPTION as description, i.TYPE as type, i.CATEGORY as category, ic.RETAILPRICE as price FROM retailproductentity rp, itementity i, item_countryentity ic where rp.ID=i.ID and i.id=ic.ITEM_ID and i.ISDELETED=FALSE and ic.COUNTRY_ID=?;";
+                ps = conn.prepareStatement(stmt);
+                ps.setLong(1, countryID);
+            }
+
+            ResultSet rs = ps.executeQuery();
+            while (rs.next()) {
+                RetailProduct rp = new RetailProduct();
+                
+                rp.setSKU(rs.getString("sku"));
+                rp.setId(rs.getLong("id"));
+                rp.setImageUrl(rs.getString("imageURL"));
+                rp.setType(rs.getString("type"));
+                rp.setDescription(rs.getString("description"));
+                rp.setCategory(rs.getString("category"));
+                rp.setName(rs.getString("name"));
+                
+                if (countryID != null) {
+                    rp.setPrice(rs.getDouble("price"));
+                }
+                
+                
+                list.add(rp);
+            }
+            GenericEntity<List<RetailProduct>> entity = new GenericEntity<List<RetailProduct>>(list) {
+            };
+            return Response
+                    .status(200)    
+                    .header("Access-Control-Allow-Origin", "*")
+                    .header("Access-Control-Allow-Headers", "origin, content-type, accept, authorization")
+                    .header("Access-Control-Allow-Credentials", "true")
+                    .header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS, HEAD")
+                    .header("Access-Control-Max-Age", "1209600")
+                    .entity(entity)
+                    .build();
+        } catch (Exception ex) {
+            ex.printStackTrace();
+            return Response.status(Response.Status.NOT_FOUND).build();
+        }
+    }
+
+    @PUT
+    @Path("{id}")
+    @Consumes({"application/xml", "application/json"})
+    public void edit(@PathParam("id") Long id, Retailproductentity entity) {
+        super.edit(entity);
+    }
+
+    @DELETE
+    @Path("{id}")
+    public void remove(@PathParam("id") Long id) {
+        super.remove(super.find(id));
+    }
+
+    @GET
+    @Path("{id}")
+    @Produces({"application/xml", "application/json"})
+    public Retailproductentity find(@PathParam("id") Long id) {
+        return super.find(id);
+    }
+
+    @GET
+    @Override
+    @Produces({"application/xml", "application/json"})
+    public List<Retailproductentity> findAll() {
+        return super.findAll();
+    }
+
+    @GET
+    @Path("{from}/{to}")
+    @Produces({"application/xml", "application/json"})
+    public List<Retailproductentity> findRange(@PathParam("from") Integer from, @PathParam("to") Integer to) {
+        return super.findRange(new int[]{from, to});
+    }
+
+    @GET
+    @Path("count")
+    @Produces("text/plain")
+    public String countREST() {
+        return String.valueOf(super.count());
+    }
+
+    @Override
+    protected EntityManager getEntityManager() {
+        return em;
+    }
+
+}
